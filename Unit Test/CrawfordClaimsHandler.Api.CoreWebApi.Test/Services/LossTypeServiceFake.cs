35beff2b0e5fbf55316090c0a79258fcbc0f220c
Saif Khan <skhan@carus.ca>
2021-08-12 13:14:46 +0530

    added unit test proj

---
+++ Unit Test/CrawfordClaimsHandler.Api.CoreWebApi.Test/Services/LossTypeServiceFake.cs
@@ -0,0 +1,29 @@
+﻿using CrawfordClaimsHandler.Api.CoreWebApi.Data.Contracts;
+using CrawfordClaimsHandler.Api.CoreWebApi.DomainEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawfordClaimsHandler.Api.CoreWebApi.Test.Services
+{
+    public class LossTypeServiceFake : ILossTypeService
+    {
+        private readonly IEnumerable<LossType> _lossTypes;
+
+        public LossTypeServiceFake(){
+            _lossTypes = new List<LossType>()
+            {
+                new LossType() {LossTypeId = 1, LossTypeCode = "A", LossTypeDescription = "desc A", Active = true},
+                new LossType() {LossTypeId = 2, LossTypeCode = "B", LossTypeDescription = "desc B", Active = true},
+                new LossType() {LossTypeId = 3, LossTypeCode = "C", LossTypeDescription = "desc C", Active = false}   
+    };
+}
+
+        public async Task<IEnumerable<LossType>> GetAllLossTypesAsync()
+        {
+            return await Task.Run( () => _lossTypes);
+        }
+    }
+}
---
+++ Unit Test/CrawfordClaimsHandler.Api.CoreWebApi.Test/TestController/LossTypeControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using CrawfordClaimsHandler.Api.CoreWebApi.Controllers;
+using CrawfordClaimsHandler.Api.CoreWebApi.Data.Contracts;
+using CrawfordClaimsHandler.Api.CoreWebApi.DomainEntities;
+using CrawfordClaimsHandler.Api.CoreWebApi.Test.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CrawfordClaimsHandler.Api.CoreWebApi.Test.TestController
+{
+    public class LossTypeControllerTest
+    {
+        LossTypeController _controller;
+        ILossTypeService _service;
+        public LossTypeControllerTest()
+        {
+            _service = new LossTypeServiceFake();
+            _controller = new LossTypeController(_service);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.GetAsync().Result;
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var result =  _controller.GetAsync().Result.Result as OkObjectResult; ;
+            // Assert
+            var items = Assert.IsType<List<LossType>>(result.Value);
+            Assert.Equal(3, items.Count);
+        }
+    }
+}
